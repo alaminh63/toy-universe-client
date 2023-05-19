63e697ae1eaad4773be54e381b4826bbbd06dbe0
AlaminHasanPro <alaminhasanpro@gmail.com>
2023-05-20 03:34:34 +0600

    first commit

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Bangla songs</div>,
+  },
+]);
+
+export default router;
