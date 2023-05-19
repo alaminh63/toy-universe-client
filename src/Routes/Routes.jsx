1aa1e9bb921b2f0b94ae25b9f993eaa3ee46ec2d
AlaminHasanPro <alaminhasanpro@gmail.com>
2023-05-20 03:37:44 +0600

    tailwind added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,7 +3,9 @@ import { createBrowserRouter } from "react-router-dom";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Bangla songs</div>,
+    element: <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>,
   },
 ]);
 
