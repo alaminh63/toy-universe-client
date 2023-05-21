4147a6c761c5a37d2b1b36508fc205f16356409e
AlaminHasanPro <alaminhasanpro@gmail.com>
2023-05-21 22:12:30 +0600

    home page updated

--- src/Pages/Home/NewCollection/NewCollection.jsx
+++ src/Pages/Home/NewCollection/NewCollection.jsx
@@ -1,8 +1,17 @@
 import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { useState } from "react";
+import NewCollectionCard from "../NewCollectionCard/NewCollectionCard";
 
 const NewCollection = () => {
+  const [collection, setCollection] = useState([]);
+  useEffect(() => {
+    fetch("https://toy-universe-server-bay.vercel.app/toys2")
+      .then((res) => res.json())
+      .then((data) => setCollection(data));
+  }, []);
+  console.log(collection);
   useEffect(() => {
     AOS.init();
     AOS.refresh();
@@ -15,183 +24,9 @@ const NewCollection = () => {
             Latest articles
           </h2>
           <div className="grid lg:grid-cols-3 gap-6">
-            <div
-              data-aos="zoom-out-right"
-              className="relative overflow-hidden bg-no-repeat bg-cover shadow-lg rounded-lg"
-              style={{ backgroundPosition: "50%" }}
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-            >
-              <img
-                src="https://mdbootstrap.com/img/new/standard/nature/051.jpg"
-                className="w-full "
-              />
-              <a href="#!">
-                <div
-                  className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed"
-                  style={{ backgroundColor: "rgba(0, 0, 0, 0.4)" }}
-                >
-                  <div className="flex justify-start items-end h-full ">
-                    <div className="text-white m-6">
-                      <h5 className="font-bold text-lg mb-3">I miss the sun</h5>
-                      <p>
-                        <small>
-                          Published <u>13.01.2022</u> by Anna Maria Doe
-                        </small>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </a>
-            </div>
-            <div
-              data-aos="zoom-out"
-              className="relative overflow-hidden bg-no-repeat bg-cover shadow-lg rounded-lg"
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-            >
-              <img
-                src="https://mdbootstrap.com/img/new/standard/nature/044.jpg"
-                className="w-full"
-              />
-              <a href="#!">
-                <div
-                  className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed"
-                  style={{ backgroundColor: "rgba(0, 0, 0, 0.4)" }}
-                >
-                  <div className="flex justify-start items-end h-full">
-                    <div className="text-white m-6">
-                      <h5 className="font-bold text-lg mb-3">
-                        Adventure in the desert
-                      </h5>
-                      <p>
-                        <small>
-                          Published <u>12.01.2022</u> by Mark Equel
-                        </small>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </a>
-            </div>
-            <div
-              data-aos="zoom-out-left"
-              className="relative overflow-hidden bg-no-repeat bg-cover shadow-lg rounded-lg"
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-            >
-              <img
-                src="https://mdbootstrap.com/img/new/standard/nature/045.jpg"
-                className="w-full"
-              />
-              <a href="#!">
-                <div
-                  className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed"
-                  style={{ backgroundColor: "rgba(0, 0, 0, 0.4)" }}
-                >
-                  <div className="flex justify-start items-end h-full">
-                    <div className="text-white m-6">
-                      <h5 className="font-bold text-lg mb-3">
-                        Lonely mountain
-                      </h5>
-                      <p>
-                        <small>
-                          Published <u>10.01.2022</u> by Bilbo baggins
-                        </small>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </a>
-            </div>
-            <div
-              data-aos="zoom-out-right"
-              className="relative overflow-hidden bg-no-repeat bg-cover shadow-lg rounded-lg"
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-            >
-              <img
-                src="https://mdbootstrap.com/img/new/standard/nature/047.jpg"
-                className="w-full"
-              />
-              <a href="#!">
-                <div
-                  className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed"
-                  style={{ backgroundColor: "rgba(0, 0, 0, 0.4)" }}
-                >
-                  <div className="flex justify-start items-end h-full">
-                    <div className="text-white m-6">
-                      <h5 className="font-bold text-lg mb-3">Let's go!</h5>
-                      <p>
-                        <small>
-                          Published <u>09.01.2022</u> by Halley Frank
-                        </small>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </a>
-            </div>
-            <div
-              data-aos="zoom-out"
-              className="relative overflow-hidden bg-no-repeat bg-cover shadow-lg rounded-lg"
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-            >
-              <img
-                src="https://mdbootstrap.com/img/new/standard/nature/028.jpg"
-                className="w-full"
-              />
-              <a href="#!">
-                <div
-                  className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed"
-                  style={{ backgroundColor: "rgba(0, 0, 0, 0.4)" }}
-                >
-                  <div className="flex justify-start items-end h-full">
-                    <div className="text-white m-6">
-                      <h5 className="font-bold text-lg mb-3">
-                        A hut in the mountains
-                      </h5>
-                      <p>
-                        <small>
-                          Published <u>07.01.2022</u> by David Beak
-                        </small>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </a>
-            </div>
-            <div
-              data-aos="zoom-out-left"
-              className="relative overflow-hidden bg-no-repeat bg-cover shadow-lg rounded-lg"
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-            >
-              <img
-                src="https://mdbootstrap.com/img/new/standard/nature/049.jpg"
-                className="w-full"
-              />
-              <a href="#!">
-                <div
-                  className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed"
-                  style={{ backgroundColor: "rgba(0, 0, 0, 0.4)" }}
-                >
-                  <div className="flex justify-start items-end h-full">
-                    <div className="text-white m-6">
-                      <h5 className="font-bold text-lg mb-3">
-                        Beautiful waterfall
-                      </h5>
-                      <p>
-                        <small>
-                          Published <u>04.01.2022</u> by Joe Svan
-                        </small>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </a>
-            </div>
+            {collection.map((item) => (
+              <NewCollectionCard item={item}></NewCollectionCard>
+            ))}
           </div>
         </section>
       </div>
